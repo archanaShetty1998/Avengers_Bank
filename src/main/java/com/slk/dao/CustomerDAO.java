fb1f0f1fb336d03daba239f77809e8d3c8c83644
slk <slk@10.60.24.99>
2019-12-03 15:10:24 +0530

    backend

---
+++ src/main/java/com/slk/dao/CustomerDAO.java
@@ -0,0 +1,24 @@
+package com.slk.dao;
+
+
+import java.util.List;
+import org.springframework.stereotype.Repository;
+
+import com.slk.model.Customer;
+import com.slk.model.Loan;
+import com.slk.model.Transaction;
+
+@Repository
+public interface CustomerDAO {
+	Customer viewProfile(int id);
+	List<Customer> viewAllCustomer();
+	public double viewBalance(int id);
+	Loan viewLoan(int id);
+	void updateprofile(int id,Customer cust);
+	List list();
+	List<Transaction> transactionhistory(int id);
+	public int transfer(int id, double amount, int account_number);
+	public int loginValidation(String username,String password);
+
+	
+}
